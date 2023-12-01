74a8847fa07e3954dec619b42855df22ac5c219d
Andrew Mosh <andrewmosh@MacBook-Air-Andrew.local>
2023-12-01 20:18:09 +0300

    formulae is cjanged

--- src/utils/CalculateBoxes.js
+++ src/utils/CalculateBoxes.js
@@ -1,11 +1,19 @@
-export function calculateBoxes(height=60, length=40, width=40, quantity=0) {
-    const maxBoxSize = { height: 60, length: 40, width: 40 };
-    const boxVolume = maxBoxSize.height * maxBoxSize.length * maxBoxSize.width;
-    
-    const productVolume = height * length * width * quantity;
-    
-    const totalBoxes = Math.ceil(productVolume / boxVolume);
-    
+export function calculateBoxes(height, length, width, quantity) {
+    const boxHeight = 60;
+    const boxLength = 40;
+    const boxWidth = 40;
+    let totalBoxes = 0;
+  
+    while (quantity > 0) {
+      const boxVolume = boxHeight * boxLength * boxWidth;
+      const itemVolume = height * length * width;
+  
+      const itemsInBox = Math.floor(boxVolume / itemVolume);
+      const boxesNeeded = Math.ceil(quantity / itemsInBox);
+  
+      totalBoxes += boxesNeeded;
+      quantity -= itemsInBox * boxesNeeded;
+    }
+  
     return totalBoxes;
-}
- 
+  }
