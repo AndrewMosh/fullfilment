55a5286b1aa3ad59049ab283fff9167c7861e616
Andrew Mosh <andrewmosh@MacBook-Air-Andrew.local>
2023-12-02 22:52:57 +0300

    bug fixed

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -16,13 +16,13 @@ const Calculator = () => {
     const checkbox = e.target;
     const price = parseFloat(checkbox.getAttribute('data-price'));
     if (checkbox.checked) {
-      if(all){
+      if(all===true){
         setTotal((prevTotal) => (price*itemsQuantity)+prevTotal);
       }else {
         setTotal((prevTotal) => (prevTotal + price));
       }
     } else {
-      if(all){
+      if(all===true){
         setTotal((prevTotal) => prevTotal - (price * itemsQuantity));
       }else {
         setTotal((prevTotal) => (prevTotal - price));
@@ -31,7 +31,11 @@ const Calculator = () => {
     }
   };
 const pricePerBox= itemsQuantity<1000?160:itemsQuantity>1001 && itemsQuantity<2001?110:100
-
+const handleQuantity = (e) => {
+  setItemsQuantity( e.target.value)
+            setBoxQuantity(() => calculateBoxes(height, length, width, e.target.value))
+            setTotal( pricePerBox*boxQuantity)
+}
 
 useEffect(() => {
   setBoxQuantity(() => calculateBoxes(height, length, width, itemsQuantity))
@@ -57,11 +61,8 @@ useEffect(() => {
         <input placeholder="высота" type="number" id="height" value={height} onChange={(e) => setHeight(e.target.value)} />
         <input placeholder="ширина" type="number" id="width" value={width} onChange={(e) => setWidth(e.target.value)} />
         <input placeholder="длина" type="number" id="length" value={length} onChange={(e) => setLength(e.target.value)} />
-        <input  placeholder="количество единиц" type="number" id="quantity" value={itemsQuantity} onChange={(e) => {
-            setItemsQuantity( e.target.value)
-            setBoxQuantity(() => calculateBoxes(height, length, width, e.target.value))
-            setTotal( pricePerBox*boxQuantity)
-        } } /> <br />
+        <input  placeholder="количество единиц" type="number" id="quantity" value={itemsQuantity} onChange={
+          handleQuantity} /> <br />
 {PACKAGE.map((item) => (
   <label key={item.name}> 
   <input type="checkbox" data-price={itemsQuantity>1000?item.to1000:itemsQuantity>1001 && itemsQuantity<2001?item.from1001:item.from2001}  onChange={(e)=>handleCheckboxChange(e,item.all)} /> {item.name} </label>
