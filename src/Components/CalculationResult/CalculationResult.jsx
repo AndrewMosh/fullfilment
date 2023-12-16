b0a7c94179a09f936b33ba6ef092a1e8ed50ab5b
Andrew Mosh <andrewmosh@MacBook-Air-Andrew.local>
2023-12-16 20:39:50 +0300

    deploy

--- src/Components/CalculationResult/CalculationResult.jsx
+++ src/Components/CalculationResult/CalculationResult.jsx
@@ -92,7 +92,7 @@ const CalculationResult = ({handleRecalculate, width, height, lengthBox, itemsQu
 
     return (
 <div className={styles.result}>
-    <div className={styles.titleContainer}><div className={styles.titleFlex}><div className={styles.title}>Примерная стоимость</div><div className={styles.price}>{formatPrice(total)} ₽</div></div>
+    <div className={styles.titleContainer}><div className={styles.titleFlex}><div className={styles.title}>Примерная стоимость</div><div className={styles.price}>{formatPrice(total)}₽</div></div>
     <ButtonTransparentDownload buttonText='Скачать прайс-лист'/>
     </div>
     <div>
@@ -100,21 +100,21 @@ const CalculationResult = ({handleRecalculate, width, height, lengthBox, itemsQu
     <h2>Информация о товаре</h2></div>
     <div className={styles.info} >
 <ResultCard title='Габариты' perUnit='(ДхВхШсм):' value={`${lengthBox}x${height}x${width}`}/>
-<ResultCard title='Количество' perUnit='(шт):' value={itemsQuantity}/>
-<ResultCard title='Коробки' perUnit='(примерно шт):' value={boxQuantity}/>
+<ResultCard title='Количество' perUnit='(шт):' value={formatPrice(itemsQuantity)}/>
+<ResultCard title='Коробки' perUnit='(примерно шт):' value={formatPrice(boxQuantity)}/>
     </div>
     </div>
     <div>
-        <div className={styles.subhead}><div style={{display:'flex', alignItems:'center'}}><img src={pack} alt="package" /><h2>Упаковка </h2></div><div className={styles.priceSeparated}>{formatPrice(allPackages)}  ₽</div></div>
+        <div className={styles.subhead}><div style={{display:'flex', alignItems:'center'}}><img src={pack} alt="package" /><h2>Упаковка </h2></div><div className={styles.priceSeparated}>{formatPrice(allPackages)}₽</div></div>
     <div className={styles.info}>
-{packages?packages.filter(item=>item.applied===true && item.all===true).map((item)=>(<ResultCard key={item.id} title={item.name} perUnit={`${calculatePerUnit(itemsQuantity, item)} ₽/шт`} value={`${formatPrice(multiplyBoxes(itemsQuantity, item,boxQuantity, item.id!==15?false:true))} ₽`}/>)):null}
+{packages?packages.filter(item=>item.applied===true && item.all===true).map((item)=>(<ResultCard key={item.id} title={item.name} perUnit={`${calculatePerUnit(itemsQuantity, item)}₽/шт`} value={`${formatPrice(multiplyBoxes(itemsQuantity, item,boxQuantity, item.id!==15?false:true))}₽`}/>)):null}
     </div>
     </div>
     <div>
-        <div className={styles.subhead}><div style={{display:'flex', alignItems:'center'}}><img src={transfer} alt="logistics" /><h2>Логистика</h2></div> <div className={styles.priceSeparated}>{formatPrice(totalLogistics)} ₽</div> </div>
+        <div className={styles.subhead}><div style={{display:'flex', alignItems:'center'}}><img src={transfer} alt="logistics" /><h2>Логистика</h2></div> <div className={styles.priceSeparated}>{formatPrice(totalLogistics)}₽</div> </div>
     <div className={styles.info}>
-    {logistics?logistics.filter(item=>item.applied===true).map((item)=>(<ResultCardWithLogistics key={item.id} title={item.name} price={`${formatPrice(item.price * boxQuantity)} ₽`} place={item.place} />)):null}
-    {packages?packages.filter(item=>item.all===false && item.applied===true).map((item)=>(<ResultCardWithLogistics key={item.id} title={item.nameForCard} price={`${formatPrice(item.to1000)} ₽`} place={item.place} weight={item.weight} info={item.info} />)):null}
+    {logistics?logistics.filter(item=>item.applied===true).map((item)=>(<ResultCardWithLogistics key={item.id} title={item.name} price={`${formatPrice(item.price * boxQuantity)}₽`} place={item.place} />)):null}
+    {packages?packages.filter(item=>item.all===false && item.applied===true).map((item)=>(<ResultCardWithLogistics key={item.id} title={item.nameForCard} price={`${formatPrice(item.to1000)}₽`} place={item.place} weight={item.weight} info={item.info} />)):null}
     </div>
     </div>
 <ButtonRecalculate link='#calculator' onClick={handleRecalculate}>Рассчитать заново</ButtonRecalculate>
