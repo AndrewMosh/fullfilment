58d3710a20014b5e70f6a92dccaeec2c48112ada
Andrew Mosh <andrewmosh@MacBook-Air-Andrew.local>
2023-12-20 21:16:38 +0300

    deploy

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -78,7 +78,6 @@ const Calculator = () => {
     setLengthBox('');
     setBoxQuantity('');
     setTotal('');
-    
     setLogistics(LOGISTICS);
     setPackages(PACKAGE);
     setError({ params: false, logistics: false });
@@ -194,7 +193,7 @@ const Calculator = () => {
           <div className={styles.popup}>
             <div className={styles.popupOverlay} style={{display:isModalOpen ? 'block' : 'none'}}></div>
             <div
-              style={{ border: isModalOpen ? '2px solid #0250EE' : error.logistics ? '2px solid #DB063B' : '2px solid #A9B0BE' }}
+              style={{transition:isModalOpen ? 'all 0.8s ease' : 'all 0.3s ease', border: isModalOpen ? '2px solid #0250EE' : error.logistics ? '2px solid #DB063B' : '2px solid #A9B0BE' }}
               className={styles.inputContainer}
              
               onClick={handleButtonClick}
