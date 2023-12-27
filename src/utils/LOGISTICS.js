050b4a8f2efad51e8c8d31eddb98e72030cad002
Andrew Mosh <malinaenglishclub@gmail.com>
2023-12-27 23:43:25 +0300

    deploy

--- src/utils/LOGISTICS.js
+++ src/utils/LOGISTICS.js
@@ -18,17 +18,17 @@ export const LOGISTICS = [
 
       
     },
+    // {
+    //     id:3,
+    //     name:'Wildberries',
+    //     place:'г. Подольск, ул. Поливановская, д.9',
+    //     price: 320,
+    //     per:'за 1 коробку',
+    //     applied:false,
+      
+    // },
     {
         id:3,
-        name:'Wildberries',
-        place:'г. Подольск, ул. Поливановская, д.9',
-        price: 320,
-        per:'за 1 коробку',
-        applied:false,
-      
-    },
-    {
-        id:4,
         name:'OZON',
         place:'​Московская область, д. Хоругвино, 32/2',
         price: 430,
@@ -36,5 +36,42 @@ export const LOGISTICS = [
         applied:false,
     
     },
+    {
+        id:4,
+        name:'OZON',
+        place:'г. Пушкино, Ярославское шоссе, д. 216, 32/2',
+        price: 430,
+        per:'за 1 коробку',
+        applied:false,
+    
+    },
+    {
+        id:5,
+        name:'OZON',
+        place:'г. Жуковский, д. 457, стр. 5 ',
+        price: 320,
+        per:'за 1 коробку',
+        applied:false,
+    
+    },
+    {
+        id:6,
+        name:'OZON',
+        place:'с. Петровское, территория МПСК Ориентир-Запад, зд. 1А.',
+        price: 430,
+        per:'за 1 коробку',
+        applied:false,
+    
+    },
+    {
+        id:7,
+        name:'OZON',
+        place:'с. Павловская Слобода, территория квартала 0050343, зд. 2',
+        price: 430,
+        per:'за 1 коробку',
+        applied:false,
+    
+    },
+    
   
 ]
